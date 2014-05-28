93f8eb55e29699cda0618e4c73a9c16d8134b837
Kevin Wilson <kevin@knewton.com>
2014-05-28 15:48:24 -0400

    Getting travis set up for the project

--- setup.py
+++ setup.py
@@ -1,4 +1,20 @@
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+import sys
+
+class PyTest(TestCommand):
+    """ This class is copied directly from the docs """
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
 
 setup(
     name = "summonses",
@@ -11,5 +27,7 @@ setup(
     url = "https://github.com/khwilson/summonses",
     packages=['summonses'],
     long_description=open('README.rst', 'r').read(),
-    install_requires=open('requirements.txt', 'r').readlines()
+    install_requires=open('requirements.txt', 'r').readlines(),
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest}
 )
